a5cb44ed972b42314cb05c7d1edfa83ae1c0a7cd
Alessandro Maclaine <almaclaine@gmail.com>
2022-01-17 01:01:17 -0600

    add player/background

---
+++ src/app.rs
@@ -0,0 +1,35 @@
+use crate::player::Player;
+use crate::prelude::*;
+
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(
+                Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            ),
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
+
+pub fn app() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler") .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::all, clippy::pedantic)]
+
+use bracket_lib::prelude::BError;
+
+mod map;
+mod app;
+mod player;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+fn main() -> BError {
+    app::app()
 }
